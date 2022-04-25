d707b77a80878d14feabb0d69d9fb321008a0b25
Lucas Paiva <lucas.paiva@mobile2you.com.br>
2022-04-25 14:48:18 -0300

    Initial Commit

---
+++ Grand Central Dispatch Test App/Grand_Central_Dispatch_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grand_Central_Dispatch_Test_AppApp.swift
+//  Grand Central Dispatch Test App
+//
+//  Created by Mobile2you on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Grand_Central_Dispatch_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
