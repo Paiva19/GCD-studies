cb9bba2731049437739e1fd1b92fac712e300da7
Lucas Paiva <lucas.paiva@mobile2you.com.br>
2022-04-27 11:43:08 -0300

    Add parallelism code to study different types of queues, race conditions, and dispatch groups

--- Grand Central Dispatch Test App/ContentView.swift
+++ Grand Central Dispatch Test App/ContentView.swift
@@ -10,68 +10,263 @@ import Combine
 
 struct ContentView: View {
     
+    // MARK: - Variables
     let viewModel: MoviePosterViewModel = MoviePosterViewModel()
     
+    // MARK: - Layout Variables
     @State var posterImage: UIImage = UIImage(named: "madagascar")!
-    @State var counterButtonLabel = "Will run on: "
+    @State var posterTitle: String = "madagascar"
+    @State var queueTypeButtonLabel = "Will run on: \(QueueType.noQueue.rawValue)"
     
     private func didTapCounterButton() {
-        counterButtonLabel = "Will run on: \(viewModel.switchThreadType())"
+        queueTypeButtonLabel = "Will run on: \(viewModel.switchQueueType())"
     }
     
+    // MARK: - Poster methods
     //    Fixes the poster showed to be the Shrek poster
-        func fixPoster() {
-            switch viewModel.threadType {
-            case .noThread:
-                fixPosterWithoutThread()
-            case .mainThread:
-                fixPosterUsingMain()
-            case .backgroundThread:
-                fixPosterUsingBackground()
-            }
+    private func fixPoster() {
+        switch viewModel.queueType {
+        case .noQueue:
+            changePosterWithoutQueue()
+        case .main:
+            changePosterUsingMain()
+        case .background:
+            changePosterUsingBackground()
+        case .serialQueue:
+            useSerialQueue()
+        case .concurrentQueue:
+            useConcurrentQueue()
+        case .concurrentQueueBarrier:
+            useConcurrentQueueWithBarrier()
+        case .concurrentQueueSemaphore:
+            useConcurrentQueueWithSemaphore()
+        case .dispatchGroup:
+            useDispatchGroup()
+        case .dispatchGroupWithArray:
+            useDispatchGroupWithArray()
         }
-        
-    
-    private let dispatchQueue = DispatchQueue(label: "DQueue")
-        
-    
-    private func fixPosterWithoutThread() {
-        changePoster(newPoster: viewModel.getOtherPoster())
     }
-        private func fixPosterUsingMain() {
-            DispatchQueue.main.async {
-                changePoster(newPoster: viewModel.getOtherPoster())
-            }
-        }
-        
-        private func fixPosterUsingBackground() {
-            dispatchQueue.async {
-                changePoster(newPoster: viewModel.getOtherPoster(), delay: 10)
-            }
-            
-            dispatchQueue.async {
-                changePoster(newPoster: viewModel.getOtherPoster())
-            }
-        }
     
     private func changePoster(newPoster: Poster, delay: UInt32 = 3) {
+        // We write to a shared resource
         viewModel.currentPoster = newPoster
+        
+        posterTitle = viewModel.currentPoster.rawValue
+        
+        // Simulate that this block takes a while to execute...
+        // Might be some calculation that takes a while, an API request...
         sleep(delay)
+        
+        // We read that shared resource to update our poster
         if let image = UIImage(named: viewModel.currentPoster.rawValue) {
             posterImage = image
         }
     }
     
+    // MARK: - Parallelism
+    // Serial Queue
+    private let dispatchQueue = DispatchQueue(label: "DQueue")
+    
+    // Concurrent Queue
+    private let concurrentQueue = DispatchQueue(label: "CQueue", attributes: .concurrent)
+    
+    // Dispatch Group
+    private let group = DispatchGroup()
+
+    // MARK: - With different types of queue
+    // No queueing task. Using main thread
+    private func changePosterWithoutQueue() {
+        print("Iniciando Tarefa: \(Thread.current)")
+        changePoster(newPoster: viewModel.getOtherPoster())
+        print("Finalizando Tarefa: \(Thread.current)")
+    }
+    
+    // Queueing task on main thread
+    private func changePosterUsingMain() {
+        print("Adicionando tarefa na fila da thread principal")
+        DispatchQueue.main.async {
+            print("Iniciando tarefa: \(Thread.current)")
+            changePoster(newPoster: viewModel.getOtherPoster())
+            print("Finalizando tarefa: \(Thread.current)")
+        }
+    }
+    
+    // Queueing task on a random background thread
+    private func changePosterUsingBackground() {
+        print("Adicionando tarefa síncrona na fila da thread no plano de fundo")
+        dispatchQueue.async {
+            print("Iniciando Tarefa: \(Thread.current)")
+            changePoster(newPoster: viewModel.getOtherPoster())
+            print("Finalizando Tarefa: \(Thread.current)")
+        }
+    }
+
+    
+    // MARK: - Serial vs Concurrent queues
+    private func useSerialQueue() {
+        print("Adicionando tarefa 1 na fila da thread no plano de fundo")
+        dispatchQueue.async {
+            print("Iniciando Tarefa 1: \(Thread.current)")
+            changePoster(newPoster: .madagascar, delay: 10)
+            print("Finalizando Tarefa 1: \(Thread.current)")
+        }
+        
+        print("Adicionando tarefa 2 na fila da thread no plano de fundo")
+        dispatchQueue.async {
+            print("Iniciando tarefa 2: \(Thread.current)")
+            changePoster(newPoster: .nemo, delay: 5)
+            print("Finalizando Tarefa 2: \(Thread.current)")
+        }
+    }
+    
+    private func useConcurrentQueue() {
+        print("Adicionando Tarefa: Paralela 1")
+        concurrentQueue.async {
+            print("Iniciando Tarefa 1: \(Thread.current)")
+            changePoster(newPoster: .shrek, delay: 10)
+            print("Finalizando Tarefa 1:  \(Thread.current)")
+        }
+        
+        print("Adicionando Tarefa: Paralela 2")
+        concurrentQueue.async {
+            print("Iniciando Tarefa 2: \(Thread.current)")
+            changePoster(newPoster: .nemo, delay: 15)
+            print("Finalizando Tarefa 2:  \(Thread.current)")
+        }
+    }
+    
+    // MARK: - Race condition handling
+    private func useConcurrentQueueWithBarrier() {
+        print("Adicionando Tarefa: Paralela 1")
+        concurrentQueue.async(flags: .barrier) {
+            print("Iniciando Tarefa 1: \(Thread.current)")
+            changePoster(newPoster: .shrek, delay: 10)
+            print("Finalizando Tarefa 1: \(Thread.current)")
+        }
+        
+        print("Adicionando Tarefa: Paralela 2")
+        concurrentQueue.async {
+            print("Iniciando Tarefa 2: \(Thread.current)")
+            changePoster(newPoster: .nemo, delay: 5)
+            print("Finalizando Tarefa 2: \(Thread.current)")
+        }
+    }
+    
+    private let semaphore = DispatchSemaphore(value: 2)
+    
+    private func useConcurrentQueueWithSemaphore() {
+        print("Adicionando Tarefa: Paralela 1")
+        concurrentQueue.async() {
+            print("Iniciando Tarefa 1: \(Thread.current)")
+            
+            // Indicates we want to use the shared resource
+            semaphore.wait()
+            print("Tarefa 1 Usando o recurso")
+            
+            changePoster(newPoster: .madagascar, delay: 10)
+            
+            // Indicates we are done using the shared semaphore
+            print("Finalizando Tarefa 1: \(Thread.current)")
+            semaphore.signal()
+        }
+        
+        print("Adicionando Tarefa: Paralela 2")
+        concurrentQueue.async {
+            print("Iniciando Tarefa 2: \(Thread.current)")
+            
+            viewModel.soma = 5
+            
+            // Indicates we want to use the shared resource
+            semaphore.wait()
+            print("Tarefa 2 Usando o recurso")
+            
+            changePoster(newPoster: .nemo, delay: 5)
+            
+            // Indicates we are done using the shared resource
+            print("Finalizando Tarefa 2: \(Thread.current)")
+            semaphore.signal()
+        }
+    }
+    
+    // MARK: - Dispatch Group
+    private func useDispatchGroup() {
+        // We need the task to enter the DispatchGroup
+        print("Adicionando Tarefa 1")
+        group.enter()
+        
+        concurrentQueue.async {
+            // We perform the task
+            semaphore.wait()
+            print("Iniciando Tarefa 1: \(Thread.current)")
+            changePoster(newPoster: .madagascar, delay: 10)
+            
+            // After we are done, we need the task to leave the group
+            print("Finalizando Tarefa 1:  \(Thread.current)")
+            semaphore.signal()
+            group.leave()
+        }
+        
+        // We need the task to enter the DispatchGroup
+        print("Adicionando Tarefa 2")
+        group.enter()
+        
+        concurrentQueue.async {
+            // We perform the task
+            semaphore.wait()
+            print("Iniciando Tarefa 2: \(Thread.current)")
+            changePoster(newPoster: .nemo, delay: 5)
+            
+            // After we are done, we need the task to leave the group
+            print("Finalizando Tarefa 2: \(Thread.current)")
+            semaphore.signal()
+            group.leave()
+        }
+        
+        group.notify(queue: dispatchQueue) {
+            // After all tasks are done, we end up here! :)
+            print("Finalizando DispatchGroup: \(Thread.current)")
+        }
+    }
+    
+    private func useDispatchGroupWithArray() {
+        let posters: [Poster] = [.madagascar, .nemo, .shrek]
+        
+        for (index, poster) in posters.enumerated() {
+            // We need the task to enter the DispatchGroup
+            print("Adicionando tarefa \(index)")
+            group.enter()
+            
+            concurrentQueue.async {
+                // We perform the task
+                semaphore.wait()
+                print("Iniciando tarefa \(index): \(Thread.current)")
+                changePoster(newPoster: poster, delay: UInt32(index))
+                
+                // After we are done, we need the task to leave the group
+                print("Finalizando tarefa \(index): \(Thread.current)")
+                semaphore.signal()
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: dispatchQueue) {
+            // After all tasks are done, we end up here! :)
+            print("Finalizando DispatchGroup")
+        }
+    }
+    
+    // MARK: - Layout
     var body: some View {
         
         VStack {
             
-            Button(counterButtonLabel, action: {
+            Button(queueTypeButtonLabel, action: {
                 didTapCounterButton()
             })
                 .padding(.top, 32)
+                .font(.system(size: 12, weight: .bold, design: .default))
             
-            Text("Shrek")
+            Text(posterTitle)
                 .padding()
             
             Image(uiImage: posterImage)
@@ -89,6 +284,7 @@ struct ContentView: View {
     }
 }
 
+// MARK: - Live preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Grand Central Dispatch Test App/MoviePosterViewModel.swift
+++ Grand Central Dispatch Test App/MoviePosterViewModel.swift
@@ -8,40 +8,69 @@
 import Foundation
 import Combine
 
-enum ThreadType: String {
-    case noThread = "no async thread"
-    case mainThread = "main thread"
-    case backgroundThread = "background thread"
+enum QueueType: String {
+    case noQueue = "no queue"
+    case main = "main thread queue"
+    case background = "background thread queue"
+    case serialQueue = "serial queue"
+    case concurrentQueue = "concurrent queue"
+    case concurrentQueueBarrier = "concurrent queue with barrier"
+    case concurrentQueueSemaphore = "concurrent queue with semaphore"
+    case dispatchGroup = "dispatch group"
+    case dispatchGroupWithArray = "dispatch group with array"
 }
 
 enum Poster: String {
     case madagascar
     case shrek
+    case nemo
 }
 
 class MoviePosterViewModel {
     
+    // Shared resource that represents the current poster
     var currentPoster: Poster = .madagascar
 
+    var soma = 0
+    
+    // Get a poster that is different from the one we have
     func getOtherPoster() -> Poster {
         switch currentPoster {
             case .shrek:
                 return .madagascar
             case .madagascar:
+                return .nemo
+            case .nemo:
                 return .shrek
         }
     }
     
-    var threadType: ThreadType = .mainThread
-    func switchThreadType() -> String {
-        switch threadType {
-        case .mainThread:
-            threadType = .backgroundThread
-        case .backgroundThread:
-            threadType = .noThread
-        case .noThread:
-            threadType = .mainThread
+    
+///     Selecting which type of thread we want to queue our task on
+///     For demonstration purposes
+///     In a real world application, this would not exist
+    var queueType: QueueType = .noQueue
+    func switchQueueType() -> String {
+        switch queueType {
+        case .noQueue:
+            queueType = .main
+        case .main:
+            queueType = .background
+        case .background:
+            queueType = .serialQueue
+        case .serialQueue:
+            queueType = .concurrentQueue
+        case .concurrentQueue:
+            queueType = .concurrentQueueBarrier
+        case .concurrentQueueBarrier:
+            queueType = .concurrentQueueSemaphore
+        case .concurrentQueueSemaphore:
+            queueType = .dispatchGroup
+        case .dispatchGroup:
+            queueType = .dispatchGroupWithArray
+        case .dispatchGroupWithArray:
+            queueType = .noQueue
         }
-        return threadType.rawValue
+        return queueType.rawValue
     }
 }
